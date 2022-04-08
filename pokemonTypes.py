525aeaefe0c2350b269faecacb6c1f6c5f1a8b01
LuccaPaschoali <luccapaschoali07@gmail.com>
2022-04-08 12:51:30 -0300

    added lowStatus and bestSpeed filters

--- pokemonTypes.py
+++ pokemonTypes.py
@@ -7,17 +7,34 @@ df['status'] = df['attack'] + df['defense'] + df['sp_attack'] + df['sp_defense']
 
 #Returns the name of the pokemons with the best status, if there is a tie code will return both names
 def BestPokemon():
-    dfTemp = df.sort_values(by = 'status', axis=0, ascending=False, inplace=False, kind='quicksort', na_position='last')
-    topStatus = dfTemp['status'].head(1)
+    topStatus = max(df['status'])
     topStatusCount = (df.status.values == int(topStatus)).sum()
+    dfTemp = df.sort_values(by = 'status', axis=0, ascending=False, inplace=False, kind='quicksort', na_position='last')
     topStatusPokemon = dfTemp[['name', 'status']].head(int(topStatusCount))
     return topStatusPokemon
 
 #pokemons with low-tier status
+def WorstPokemon():
+    lowStatus = min(df['status'])
+    lowStatusCount = (df.status.values == int(lowStatus)).sum()
+    dfTemp = df.sort_values(by = 'status', axis=0, ascending=True, inplace=False, kind='quicksort', na_position='first')
+    lowStatusPokemon = dfTemp[['name', 'status']].head(int(lowStatusCount))
+    return lowStatusPokemon
+
 #which is the fastest type?
+def FastestPokemon():
+    bestSpeed = max(df['speed'])
+    bestSpeedCount = (df.speed.values ==int(bestSpeed)).sum()
+    dfTemp = df.sort_values(by = 'speed', axis=0, ascending=False, inplace=False, kind='quicksort', na_position='last')
+    bestSpeedPokemon = dfTemp[['name', 'speed']].head(int(bestSpeedCount))
+    return bestSpeedPokemon
+
 #which is the happiest type?
 #which pokemons are the hardest to catch?
 #which is the weakest type?
 #which is the strongest type?
 
 
+print(WorstPokemon())
+print(BestPokemon())
+print(FastestPokemon())
