9ffba12682e3a2cb2adda000db9867e579792560
LuccaPaschoali <luccapaschoali07@gmail.com>
2022-04-09 01:48:27 -0300

    added happiest pokemon function

--- pokemonTypes.py
+++ pokemonTypes.py
@@ -10,7 +10,7 @@ def BestPokemon():
     topStatus = max(df['status'])
     topStatusCount = (df.status.values == int(topStatus)).sum()
     dfTemp = df.sort_values(by = 'status', axis=0, ascending=False, inplace=False, kind='quicksort', na_position='last')
-    topStatusPokemon = dfTemp[['name', 'status']].head(int(topStatusCount))
+    topStatusPokemon = dfTemp[['name']].head(int(topStatusCount)).to_string(index=False, header=False)
     return topStatusPokemon
 
 #pokemons with low-tier status
@@ -18,23 +18,25 @@ def WorstPokemon():
     lowStatus = min(df['status'])
     lowStatusCount = (df.status.values == int(lowStatus)).sum()
     dfTemp = df.sort_values(by = 'status', axis=0, ascending=True, inplace=False, kind='quicksort', na_position='first')
-    lowStatusPokemon = dfTemp[['name', 'status']].head(int(lowStatusCount))
+    lowStatusPokemon = dfTemp[['name']].head(int(lowStatusCount)).to_string(index=False, header=False)
     return lowStatusPokemon
 
 #which is the fastest type?
 def FastestPokemon():
     bestSpeed = max(df['speed'])
-    bestSpeedCount = (df.speed.values ==int(bestSpeed)).sum()
+    bestSpeedCount = (df.speed.values == int(bestSpeed)).sum()
     dfTemp = df.sort_values(by = 'speed', axis=0, ascending=False, inplace=False, kind='quicksort', na_position='last')
-    bestSpeedPokemon = dfTemp[['name', 'speed']].head(int(bestSpeedCount))
+    bestSpeedPokemon = dfTemp[['name']].head(int(bestSpeedCount)).to_string(index=False, header=False)
     return bestSpeedPokemon
 
 #which is the happiest type?
+def HappiestPokemon():
+    happiestStats = max(df['base_happiness'])
+    bestHappinessCount = (df.base_happiness.values == int(happiestStats)).sum()
+    dfTemp = df.sort_values(by = 'base_happiness', axis=0, ascending=False, inplace=False, kind='quicksort', na_position='last')
+    HappiestPokemon = dfTemp[['name']].head(int(bestHappinessCount)).to_string(index=False, header=False)
+    return HappiestPokemon
+
 #which pokemons are the hardest to catch?
 #which is the weakest type?
 #which is the strongest type?
-
-
-print(WorstPokemon())
-print(BestPokemon())
-print(FastestPokemon())
